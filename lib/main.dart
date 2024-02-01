09ae90e0d770dd957bfa243784307b9e30d5d1b9
IwanPWK <koadhyow@gmail.com>
2024-02-01 14:03:54 +0700

    Audio handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,45 @@
-import 'package:flutter/material.dart';
 import 'package:atomsbox/atomsbox.dart';
+import 'package:audio_handler/audio_handler.dart';
+import 'package:audio_service/audio_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'ui/home/views/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  AudioHandler audioHandler = await AudioService.init(
+    builder: () => MyAudioHandler(),
+    config: const AudioServiceConfig(
+        // androidNotificationChannelId: 'com.mycompany.myapp.audio',
+        // androidNotificationChannelName: 'Audio Service Demo',
+        // androidNotificationOngoing: true,
+        // androidStopForegroundOnPause: true,
+        ),
+  );
+
+  runApp(MyApp(audioHander: audioHandler));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required AudioHandler audioHander}) : _audioHandler = audioHander;
 
-  // This widget is the root of your application.
+  final AudioHandler _audioHandler;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: AppTheme.darkTheme,
-      home: const HomeScreen(),
+    return MultiRepositoryProvider(
+      providers: [
+        // RepositoryProvider<SongRepository>(
+        //   create: (context) => SongRepository(audioHandler: _audioHandler),
+        // ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: AppTheme.darkTheme,
+        home: const HomeScreen(),
+      ),
     );
   }
 }
